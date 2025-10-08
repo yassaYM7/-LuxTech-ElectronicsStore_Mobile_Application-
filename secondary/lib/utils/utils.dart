532d3644419049f6c10ef714b08a44003d1b48d2
Yassa Mouris <146125680+yassaYM7@users.noreply.github.com>
2025-10-08 09:49:19 +0300

    Delete secondary directory

--- secondary/lib/utils/utils.dart
+++
@@ -1,7 +0,0 @@
-import 'package:intl/intl.dart';
-
-/// Formats a price with thousands separators and EGP, without decimal places
-String formatPrice(double price) {
-  final formatter = NumberFormat('#,##0', 'en_US');
-  return '${formatter.format(price)} EGP';
-} 
